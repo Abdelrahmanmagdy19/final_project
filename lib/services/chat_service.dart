569d9dbc9a49455a8960cc8032576a307136dddc
Yatherdev <yather.dev@gmail.com>
2025-11-19 15:13:03 +0200

    update the file

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cure_link/models/model/user_chat_model.dart';
+import 'package:cure_link/shared/models/user_chat_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatService {
